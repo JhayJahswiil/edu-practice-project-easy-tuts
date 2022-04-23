5eee24268abd94a33f63620ffb2438eef2968d6a
JhayJahswill <50771700+JhayJahswiil@users.noreply.github.com>
2022-04-23 20:16:19 +0100

    Add files via upload

---
+++ edu/live.js
@@ -0,0 +1,46 @@
+let navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let light = document.getElementById("light");
+
+// function lightMode(){
+//     light.style.backgroundColor = "black";
+//     light.style.color = "white";
+// }
+
+// let light = document.getElementById("light");
+
+// light.oclick = function(){
+//     document.body.classList.toggle("dark-theme");
+// }
